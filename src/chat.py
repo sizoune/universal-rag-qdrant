d76bac5da086878833b05d797c66b16c63f78959
kelasdev <114830657+kelasdev@users.noreply.github.com>
2026-03-01 20:14:12 +0700

    fix: update langchain imports to support v1.2.10 (langchain_classic)

--- src/chat.py
+++ src/chat.py
@@ -1,5 +1,5 @@
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_openai import ChatOpenAI
